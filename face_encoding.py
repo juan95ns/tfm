f26cab8e36a0400d2864e5bc6a678aba67ed5e9f
Juan José Nieves Seoane <juan95ns@gmail.com>
2021-04-14 19:08:16 +0200

    face_recognition

--- face_encoding.py
+++ face_encoding.py
@@ -1,5 +1,5 @@
 # Ejecución:
-# python face_encoding.py --dataset dataset --encodings encondings\encodings.pickle
+# python face_encoding.py --dataset dataset --encodings encondings/encodings.pickle
 
 # Imports
 from imutils import paths
@@ -9,48 +9,62 @@ import pickle
 import argparse
 import face_recognition
 
-# Argumentos para la ejecución del programa
-argparser = argparse.ArgumentParser()
-argparser.add_argument("--dataset", required=True, help="path dataset")
-argparser.add_argument("--encodings", required=True, help="path output encodings")
-args = vars(argparser.parse_args())
 
-# Crear la lista de rutas de la imagen con formato:
-# dataset\\alfred_molina\\00000000.jpg
-image_paths = list(paths.list_images(args["dataset"]))
-print("[Paso 1] Rutas de las imagenes cargadas.")
+def run():
+    # Argumentos para la ejecución del programa
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--dataset", required=True, help="path dataset")
+    argparser.add_argument("--encodings", required=True, help="path output encodings")
+    args = vars(argparser.parse_args())
 
-# Listas para guardar los datos de las imagenes
-encoding_list = list()
-name_list = list()
+    # Crear la lista de rutas de la imagen con formato:
+    # dataset\\alfred_molina\\00000000.jpg
+    image_paths = list(paths.list_images(args["dataset"]))
+    print("[Paso 1] Rutas de las imagenes cargadas.")
 
-image_count = len(image_paths)
+    process_faces(args, image_paths)
 
-# Recorrer las imagenes una a una
-print("[Paso 2] Codificando caras.")
-for iteration, path in enumerate(image_paths):
-    name = path.split(os.path.sep)[-2]
 
-    # Convertir imagen a RGB
-    image = cv2.imread(path)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def process_faces(args, image_paths):
+    # Listas para guardar los datos de las imagenes
+    encoding_list = list()
+    name_list = list()
 
-    # Detectar las coordenadas que corresponden a la cara en la imagen
-    face = face_recognition.face_locations(rgb, model="cnn")
+    image_count = len(image_paths)
 
-    # Procesar la cara
-    encodings = face_recognition.face_encodings(rgb, face)
+    # Recorrer las imagenes una a una
+    print("[Paso 2] Codificando caras.")
+    for iteration, path in enumerate(image_paths):
+        name = path.split(os.path.sep)[-2]
 
-    # Poner los vectores resultantes y los nombres en la lista definitiva
-    for encoding in encodings:
-        encoding_list.append(encoding)
-        name_list.append(encoding)
+        # Convertir imagen a RGB
+        image = cv2.imread(path)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    print("Imagen {}/{}.".format(iteration + 1, image_count))
+        # Detectar las coordenadas que corresponden a la cara en la imagen
+        face = face_recognition.face_locations(rgb, model="cnn")
 
-# Escribir los encodings
-print("[Paso 3] Escribiendo encodings")
-data = {"encodings": encoding_list, "names": name_list}
-f = open(args["encodings"], "wb")
-f.write(pickle.dumps(data))
-f.close()
+        # Procesar la cara
+        encodings = face_recognition.face_encodings(rgb, face)
+
+        # Poner los vectores resultantes y los nombres en la lista definitiva
+        for encoding in encodings:
+            encoding_list.append(encoding)
+            name_list.append(encoding)
+
+        print("Imagen {}/{}.".format(iteration + 1, image_count))
+
+    write_encodings(args, encoding_list, name_list)
+
+
+def write_encodings(args, encoding_list, name_list):
+    # Escribir los encodings
+    print("[Paso 3] Escribiendo encodings")
+    data = {"encodings": encoding_list, "names": name_list}
+    f = open(args["encodings"], "wb")
+    f.write(pickle.dumps(data))
+    f.close()
+
+
+if __name__ == "__main__":
+    run()
---
+++ face_recognition_image.py
@@ -0,0 +1,72 @@
+# Ejecución:
+# python face_recognition_image.py --encodings encondings/encodings.pickle --image input_images/example_01.png 
+
+# Imports
+from cv2 import cv2
+import pickle
+import argparse
+import face_recognition
+
+
+def run():
+    # Argumentos para la ejecución del programa
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--encodings", required=True, help="path encodings")
+    argparser.add_argument("--image", required=True, help="path input image")
+    args = vars(argparser.parse_args())
+
+    # Cargar los datos necesarios para la ejecución del programa y convertir la imagen de entrada a RGB
+    print("[Paso 1] Cargando datos")
+    pickle_encodings = pickle.loads(open(args["encodings"], "rb").read())
+    image = cv2.imread(args["image"])
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    detect_faces(args, pickle_encodings, image, rgb)
+
+
+def detect_faces(args, pickle_encodings, image, rgb):
+    names = list()
+
+    # Detectar las coordenadas de la imagen que corresponden con caras
+    print("[Paso 2] Procesando imagen de entrada")
+    faces = face_recognition.face_locations(rgb, model="cnn")
+    image_encodings = face_recognition.face_encodings(rgb, faces)
+
+    # Recorremos los encodings de cada una de las caras detectadas
+    print("[Paso 3] Detectando caras")
+    for image_encoding in image_encodings:
+        # Creamos un array de booleans con las comparaciones
+        comparations = face_recognition.compare_faces(pickle_encodings["encodings"], image_encoding)
+        name = "Desconocido"
+
+        if True in comparations:
+            # Guardamos el indice del array si la comparación es positiva
+            comparations_index = [i for (i, b) in enumerate(comparations) if b == True]
+            comparations_count = dict()
+
+            # Contar las veces que sale la comparación por persona
+            for i in comparations_index:
+                name = pickle_encodings["names"][i]
+                comparations_count[name] = comparations_count.get(name, 0) + 1
+          
+            # Obtener que persona obtiene el mayor resultado
+            name = max(comparations_count, key=comparations_count.get)
+
+        names.append(name)
+
+    write_faces(image, faces, names)
+
+
+def write_faces(image, faces, names):
+    # Dibujar el cuadrado de la cara con el nombre
+    for((top, right, bottom, left), name) in zip(faces, names):
+        cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    run()
